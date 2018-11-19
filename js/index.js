adfdaf4e5e5038972249f8b14d08608a8e5932be
Cassio <cassiowt@gmail.com>
2018-11-19 10:39:20 -0300

    Exercicio Js claculo da média

--- js/index.js
+++ js/index.js
@@ -14,6 +14,21 @@ function foo() {
     }
 };
 
+function media() {
+
+    var notas = new Array( 1, 9, 9, 9, 9, 9, 9, 9, 9, 10)
+    var media = 0;
+    for (var key in notas) {
+         var numeroAluno = parseInt(key) + 1;
+         document.write("<h3> a nota do aluno " +   numeroAluno  + " é " + notas[key] + "</h3>")
+         media += notas[key];
+    }
+
+    document.write("<h2> A média dos alunos é " + media / notas.length + "</h2>")
+
+};
+
+
 
 function exemplo(){
     var  DiaSemana = new Date( );
